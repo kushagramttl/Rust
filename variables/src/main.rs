ca5dfc7775e3857e2511870defbc788afdf6a3ca
Kushagra Mittal <mittal.ku@husky.neu.edu>
2020-06-27 12:34:03 -0400

    variables initial commit

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+const MAX_VAL: u32 = 50_000;
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    println!("The value of MAX_VAL is: {}", MAX_VAL);
+}
