f9f48a3f9b9890f67c670f445d1aa1b30201123d
Kushagra Mittal <mittal.ku@northeastern.edu>
2020-08-29 18:08:44 -0400

    Updated main file

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -14,14 +14,35 @@ fn main() {
 
     let user2 = build_user(String::from("test2@test.com"), String::from("user2_name"));
     println!("The second initalized user is: {}", user2.username);
-    println!("The user {} has sign in count as: {}", user2.username, user2.sign_in_count)
+    println!("The user {} has sign in count as: {}", user2.username, user2.sign_in_count);
+
+    let user3 = User {
+        email: String::from("test3@test.com"),
+        username: String::from("user3_name"),
+        ..user1
+    };
+
+    println!("The third initalized user is: {}", user3.username);
+
+    if user3.sign_in_count == user1.sign_in_count {
+        println!("The count for sign in is same for user 1 and user 3!");
+    } else {
+        println!("The count deos not match. Check initialization again!");
+    }
+
+    struct Color(i32, i32, i32);
+    let black = Color(0, 0, 0);
+
+    println!("The z value of black is: {}", black.2);
+
+
 
 }
 
 fn build_user(email: String, username: String) -> User {
     User {
-        email: email,
-        username: username,
+        email,
+        username,
         active: true,
         sign_in_count: 32
     }
