6d138b23a867ff944ad70c9615222133debe8527
Kushagra Mittal <mittal.ku@northeastern.edu>
2020-08-29 20:37:44 -0400

    added method with multiple parameter

--- structs_area/src/main.rs
+++ structs_area/src/main.rs
@@ -5,17 +5,42 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50
     };
 
-    println!("The area of the Rectangle is: {}", area(&rect1));
+    println!("The area of the Rectangle is: {} \n", area(&rect1));
 
-    println!("Rectangle info: {:?}", rect1);
+    println!("Rectangle info: {:?} \n", rect1);
 
-    println!("For a pretty output with debugging: {:#?}", rect1);
+    println!("For a pretty output with debugging: {:#?} \n", rect1);
+
+    println!("The area calculated using method in rectangle object: {}\n", rect1.area());
+
+    let rect2 = Rectangle {
+        height: 20,
+        width: 20
+    };
+
+    let rect3 = Rectangle {
+        height: 50,
+        width: 10
+    };
+
+    println!("Can rect1 hold rect2: {}\n", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3: {}\n", rect1.can_hold(&rect3))
 }
 
 fn area(rect: &Rectangle) -> u32 {
