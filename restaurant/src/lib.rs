0a5d93b6e6aa4d3cd30d3b738f9dc00f3555c183
Kushagra Mittal <mittal.ku@northeastern.edu>
2020-10-11 19:39:27 -0400

    Updated with comments on package import

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,13 @@
+/**
+ *
+ * Based on the Rust book module available at:
+ * https://doc.rust-lang.org/book/ch07-04-bringing-paths-into-scope-with-the-use-keyword.html
+ * The crates can be imported to the module by using the 'use' keyword
+ * If there are multiple modules that are to be imported form the same mod then they can be imported in the following way:
+ * use std::io::{self, Write};
+ * If all the public items defined in the path need to be imported, the the * operator can be used.
+ *
+ */
 // all items (functions, methods, structs, enums, modules, and constants) are private by default
 pub mod front_of_house {
     pub mod hosting {
@@ -24,6 +34,13 @@ pub fn eat_at_restaurant() {
 }
 
 use crate::front_of_house::hosting::add_to_waitlist;
+
+/**
+ * The import can be combined with the pub keyowrd which would look like:
+ * pub use crate::front_of_house::hosting;
+ * The default scope of import is private to the scope
+ * However, when the public keyword is used, the import is public
+ */
 
 pub fn create_waitlist() {
     add_to_waitlist();
