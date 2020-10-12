a6186108603e43b479e96640963e3be2213de820
Kushagra Mittal <mittal.ku@northeastern.edu>
2020-10-12 01:13:08 -0400

    Updated structure

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -9,31 +9,17 @@
  *
  */
 // all items (functions, methods, structs, enums, modules, and constants) are private by default
-pub mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+// pub use crate::front_of_house::hosting;
 
-        fn seat_at_table() {}
-    }
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
 
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
-}
-
-use crate::front_of_house::hosting::add_to_waitlist;
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+// }
+mod front_of_house;
+pub use crate::front_of_house::hosting::add_to_waitlist;
 
 /**
  * The import can be combined with the pub keyowrd which would look like:
