63fbbb36c4d2548f122060d3ba92253cb71ffa39
Kushagra Mittal <mittal.ku@northeastern.edu>
2020-10-12 01:25:56 -0400

    Updated comments

--- restaurant/src/front_of_house.rs
+++ restaurant/src/front_of_house.rs
@@ -1,1 +1,8 @@
+/**
+ *
+ * The below line means that the module has be loaded from a different file - hosting.rs
+ * And that the file will be inside the front_of_house folder
+ * That way a route to the dile is created.
+ *
+ */
 pub mod hosting;
